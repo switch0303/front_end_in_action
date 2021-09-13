a1e7c733934a1e610c714cbcd2feaf2e8282daa7
wucong <coci315@163.com>
2021-09-13 22:20:36 +0800

    feat: init week10 homework

---
+++ week10/element-plus/examples/views/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-button">
+    <h2>Button 按钮</h2>
+
+    <Button></Button>
+
+    <br />
+
+    <ButtonGroup>
+      <Button>左</Button>
+      <Button>中</Button>
+      <Button>右</Button>
+    </ButtonGroup>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs, computed, h } from "vue";
+import type { PropType } from "vue";
+import { Button, ButtonGroup } from "@/ele";
+
+export default defineComponent({
+  name: "PageButton",
+  components: { Button },
+  setup(props, { emit, slots }) {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
